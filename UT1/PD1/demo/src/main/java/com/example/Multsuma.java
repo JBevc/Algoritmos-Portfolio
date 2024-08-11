9b9c890b21b6344eeb7dfb66b2fee152288087c5
JBevc <141636441+JBevc@users.noreply.github.com>
2024-08-11 20:46:50 -0300

    avance

---
+++ UT1/PD1/demo/src/main/java/com/example/Multsuma.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Multsuma {
+    public static void main(String[] args) {
+        multsuma(1.0, 2.0, 3.0);
+    }
+
+    public static void multsuma(double a, double b, double c) {
+        double resultado = a*b + c;
+        System.out.println(resultado);
+
+    }
+}
