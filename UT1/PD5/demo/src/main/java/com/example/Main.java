9250276d52bee0a4f0c73b41d9f8726d34f60da8
JBevc <141636441+JBevc@users.noreply.github.com>
2024-08-12 15:37:31 -0300

    cambios

--- UT1/PD5/demo/src/main/java/com/example/Main.java
+++ UT1/PD5/demo/src/main/java/com/example/Main.java
@@ -3,15 +3,25 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
+        // PD5 ejercicio 1
+        // Imprimo todos los valores del Enum type
         Day[] dias = Day.values();
         for (Day day : dias) {
             System.out.println(day);
         }
 
-        EnumTest firstDay = new EnumTest(Day.MONDAY);
-        firstDay.tellItLikeItIs();
-        EnumTest sixthDay = new EnumTest(Day.SATURDAY);
-        sixthDay.tellItLikeItIs();
+        // EJERCICIO 4
+        String[] myFloatArray = {"13.4", "66.1"};
+        ValueOfDemo v = new ValueOfDemo();
+        v.metodo(myFloatArray);
+
+        // La salida es 
+        // a + b = 79.5
+        // a - b = -52.699997
+        // a * b = 885.7399
+        // a / b = 0.20272315
+        // a % b = 13.4
+
 
     }
 
