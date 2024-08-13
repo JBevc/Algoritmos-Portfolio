1befc28f467ebf99753bd846b89e41585fd63252
JBevc <141636441+JBevc@users.noreply.github.com>
2024-08-12 23:51:15 -0300

    cambios PD5

---
+++ UT1/PD5/Ejercicio 4/demo/src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // EJERCICIO 4
+        String[] myFloatArray = {"13.4", "66.1"};
+        
+        ValueOfDemo.metodoValueOfDemo(myFloatArray);
+        
+        // La salida es 
+        // a + b = 79.5
+        // a - b = -52.699997
+        // a * b = 885.7399
+        // a / b = 0.20272315
+        // a % b = 13.4
+
+    }
+}
