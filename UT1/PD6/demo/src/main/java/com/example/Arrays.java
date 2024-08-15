ce9732756117983a785a556b3854a6690af66631
JBevc <141636441+JBevc@users.noreply.github.com>
2024-08-14 23:18:19 -0300

    cambios

--- UT1/PD6/demo/src/main/java/com/example/Arrays.java
+++ UT1/PD6/demo/src/main/java/com/example/Arrays.java
@@ -1,27 +1,14 @@
 package com.example;
 
-/*
-  Ejercicio 4
-  Para realizar el programa deberia crear un array con enteros
-  La multiplicacion se realizaria si los arrays tienen el mismo tamaño.
-  
-  Para realizar la multiplicacion iria recorriendo un vector y multiplicando el
-  entero correspondiente en el mismo indice del  elemento actual.
-  La multiplicacion la guardaria en una variable final, la cual mostraria el resultado
-
- */
-
 public class Arrays {
   public static void multiplicacionArray(int[] v1, int[] v2){
 
     String mult = "";
 
-    // La condición es que los dos arrays tienen que ser del mismo largo
     if (v2.length != v1.length){
       System.out.println("Los arrays no son del mismo tamaño");
     } else {
       for (int i = 0; i<v1.length; i++){
-        // Se realiza la multiplicación de los vectores
         mult += (v1[i]*v2[i]);
         mult += ",";
       }
